96a2b04ae9fb5238437cc8d33dd4879ecdff1ae7
Daiken <59336456+DankenStinks@users.noreply.github.com>
2024-05-09 17:24:37 +1000

    Workshop completed

--- project/auth.py
+++ project/auth.py
@@ -21,7 +21,8 @@ def login_post():
 
     # check if the user actually exists
     # take the user-supplied password and compare it with the stored password
-    if not user or not (user.password == password):
+    #if not user or not (user.password == password):
+    if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         current_app.logger.warning("User login failed")
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
@@ -38,9 +39,11 @@ def signup():
 def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
-    password = request.form.get('password')
+    password = generate_password_hash(request.form.get('password'), method='pbkdf2', salt_length=16)
+    # password = request.form.get('password')
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
+    user = User.query.filter_by(email=email).all()
+    # user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
     if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         current_app.logger.debug("User email already exists")
